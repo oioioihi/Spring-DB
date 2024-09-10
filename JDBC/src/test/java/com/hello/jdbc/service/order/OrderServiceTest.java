51e5c86cd47f959a52c47db17a35d364945930ae
Boeun's <rnqhdms@gmail.com>
2024-09-10 23:50:41 +0900

    Transaction - 예외 발생 시 커밋, 롤백 예제

--- JDBC/src/test/java/com/hello/jdbc/service/order/OrderServiceTest.java
+++ JDBC/src/test/java/com/hello/jdbc/service/order/OrderServiceTest.java
@@ -2,9 +2,12 @@ package com.hello.jdbc.service.order;
 
 import com.hello.jdbc.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -31,4 +34,43 @@ class OrderServiceTest {
         Order findOrder = orderRepository.findById(order.getId()).get();
         assertThat(findOrder.getPayStatus()).isEqualTo("완료");
     }
+
+    @Test
+    void runtimeException() {
+        // given
+        Order order = new Order();
+        order.setUsername("예외");
+
+        // when
+        // 시스템에 문제가 생긴 경우엔 모두 롤백
+        Assertions.assertThatThrownBy(() -> orderService.order(order))
+                .isInstanceOf(RuntimeException.class);
+
+        // then
+        Optional<Order> findOrder = orderRepository.findById(order.getId());
+        assertThat(findOrder.isEmpty()).isTrue();
+
+    }
+
+    @Test
+    void bizException() {
+        // given
+        Order order = new Order();
+        order.setUsername("잔고부족");
+
+        // when
+        // 체크예외 발생 시 작업을 롤백하지 않음
+        try {
+            orderService.order(order);
+        } catch (NotEnoughMoneyException e) {
+            log.info("고객에게 잔고 부족을 알리고 별도의 계좌로 입금하도록 안내");
+        }
+
+        // then
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        assertThat(findOrder.getPayStatus()).isEqualTo("대기");
+
+    }
+
+
 }
