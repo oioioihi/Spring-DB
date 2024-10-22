7e4d95b58d5e6ee418a4c6b15576b107f5c5f75a
Boeun's <rnqhdms@gmail.com>
2024-10-22 23:50:51 +0900

    Transaction 전파 - REQUIRES_NEW

--- JDBC/src/test/java/com/hello/jdbc/propagation/BasicTransactionTest.java
+++ JDBC/src/test/java/com/hello/jdbc/propagation/BasicTransactionTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
@@ -152,6 +153,25 @@ public class BasicTransactionTest {
         assertThatThrownBy(() -> transactionManager.rollback(outerTransaction)).isInstanceOf(UnexpectedException.class);
     }
 
+
+    @Test
+    void inner_rollback_requeires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outerTransaction = transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("out.isNewTransaction = {}", outerTransaction.isNewTransaction()); //true
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute attribute = new DefaultTransactionAttribute();
+        attribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus innerTransaction = transactionManager.getTransaction(attribute);
+        log.info("inner.isNewTransaction = {}", innerTransaction.isNewTransaction()); //true
+
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(innerTransaction); // 롤백
+
+        log.info("외부 트랜잭션 커밋");
+        transactionManager.commit(outerTransaction); // 커밋
+    }
 
     private void innerTransaction() {
         log.info("내부 트랜잭션 시작");
