8a3e12e90c983a0091400f8026ea1bb0dc71d715
Boeun's <rnqhdms@gmail.com>
2024-11-10 23:42:19 +0900

    Transaction 전파 예제 : 회원로직

---
+++ JDBC/src/main/java/com/hello/propagation/Member.java
@@ -0,0 +1,30 @@
+package com.hello.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member() {
+    }
+
+    public Member(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+}
